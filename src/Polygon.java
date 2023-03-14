44295210721cb2632e0d60ae2eb4d86c4d06b406
Mayya <msalosina@mail.ru>
2023-03-14 12:01:29 +0700

    Polygon

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,2 +1,28 @@
-public class Polygon {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Polygon extends Figure{
+    protected List<Double> sides = new ArrayList<>();
+
+    protected Polygon(double... sides) throws Exception {
+        for (double side : sides) {
+            if (side <= 0){
+                throw new Exception("Такого многоугольника не существует! " +
+                        "Стороны должны быть положительными.");
+            }
+        }
+    }
+
+    public double perimeter(){
+        double perimeter = 0;
+        for (double side : this.sides){
+            perimeter += side;
+        }
+        return perimeter;
+    }
+    @Override
+    public String toString(){
+        return super.toString() + "Стороны: " + sides +
+                "; Площадь: "  + this.area() + "; Периметр: " + this.perimeter();
+    }
 }
