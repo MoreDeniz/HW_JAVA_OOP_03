61c3859d28cb822ebf291b1b10b8dfbe81f82cff
Mayya <msalosina@mail.ru>
2023-03-14 12:02:35 +0700

    Rectangular

--- src/Rectangular.java
+++ src/Rectangular.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Rectangular {
+public class Rectangular extends Polygon implements Perimeter{
+    protected double sideA;
+    private double sideB;
+    public Rectangular(double sideA, double sideB) throws Exception {
+        this.sides.add(sideA);
+        this.sides.add(sideB);
+        this.sides.add(sideA);
+        this.sides.add(sideB);
+    }
+
+    @Override
+    public double area() {
+        double s = this.sides.get(0) * this.sides.get(1);
+        return s;
+    }
 }
